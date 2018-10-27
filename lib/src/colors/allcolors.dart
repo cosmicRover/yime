ea043535ed4739677f82a009c17decdcb3feb00e
Joy Paul <41096232+GalacticWanderer@users.noreply.github.com>
2018-10-27 11:24:35 -0400

    Contains all the colors for the screens

---
+++ lib/src/colors/allcolors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  Color purple = Color.fromRGBO(117, 56, 223, 1.0);
+  Color maxWhite = Color.fromRGBO(255, 255, 255, 1.0);
+  Color lightBlack = Color.fromRGBO(51, 51, 51, 1.0);
+  Color lightGreen = Color.fromRGBO(174, 250, 60, 1.0);
+  Color lighterGrey = Color.fromRGBO(226, 226, 226, 1.0);
+  Color darkGrey = Color.fromRGBO(90, 90, 90, 1.0);
+  Color lightGrey = Color.fromRGBO(243, 243, 243, 1.0);
+  Color red = Color.fromRGBO(250, 60, 60, 1.0);
+}
