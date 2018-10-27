f94a48a5faf4f632ce276c23b136e7de877887c1
Joy Paul <41096232+GalacticWanderer@users.noreply.github.com>
2018-10-27 16:50:53 -0400

    added coluple colors

--- lib/src/colors/allcolors.dart
+++ lib/src/colors/allcolors.dart
@@ -8,5 +8,10 @@ class AppColors {
   Color lighterGrey = Color.fromRGBO(226, 226, 226, 1.0);
   Color darkGrey = Color.fromRGBO(90, 90, 90, 1.0);
   Color lightGrey = Color.fromRGBO(243, 243, 243, 1.0);
+  Color darkerGrey = Color.fromRGBO(138, 138, 138, 1.0);
+  Color darkerGrey2 = Color.fromRGBO(125, 125, 125, 1.0);
   Color red = Color.fromRGBO(250, 60, 60, 1.0);
+  Color deepPurple = Color.fromRGBO(53, 33, 77, 1.0);
+  Color deepPurple2 = Color.fromRGBO(44, 26, 65, 1.0);
+  Color lightPink = Color.fromRGBO(236, 231, 241, 1.0);
 }
