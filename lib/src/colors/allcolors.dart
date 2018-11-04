a51622099455252d768bd0fb4d8b0b9dc68175fc
Joy Paul <41096232+GalacticWanderer@users.noreply.github.com>
2018-11-04 12:44:56 -0500

    adjusted/added colors

--- lib/src/colors/allcolors.dart
+++ lib/src/colors/allcolors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   Color purple = Color.fromRGBO(117, 56, 223, 1.0);
   Color maxWhite = Color.fromRGBO(255, 255, 255, 1.0);
-  Color lightBlack = Color.fromRGBO(51, 51, 51, 1.0);
+  Color black = Color.fromRGBO(51, 51, 51, 1.0);
   Color lightGreen = Color.fromRGBO(174, 250, 60, 1.0);
   Color lighterGrey = Color.fromRGBO(226, 226, 226, 1.0);
   Color darkGrey = Color.fromRGBO(90, 90, 90, 1.0);
